da49a03a9b84695af263a8d985346097a4ec7bed
BookerXU <bookerxu@163.com>
2023-12-19 14:12:46 +0800

    bgan

---
+++ pose/bgan/bgan_train.py
@@ -0,0 +1,170 @@
+import argparse
+import os
+import numpy as np
+import cv2
+import math
+
+import torchvision.transforms as transforms
+from torchvision.utils import save_image
+
+from torch.utils.data import DataLoader
+from torchvision import datasets
+from torch.autograd import Variable
+
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+from models import Generator, Discriminator
+from utils import KPSDataset
+
+os.makedirs("images", exist_ok=True)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--n_epochs", type=int, default=200, help="number of epochs of training")
+parser.add_argument("--batch_size", type=int, default=64, help="size of the batches")
+parser.add_argument("--lr", type=float, default=0.0002, help="adam: learning rate")
+parser.add_argument("--b1", type=float, default=0.5, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--b2", type=float, default=0.999, help="adam: decay of first order momentum of gradient")
+parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
+parser.add_argument("--latent_dim", type=int, default=10, help="dimensionality of the latent space")
+parser.add_argument("--img_size", type=int, default=28, help="size of each image dimension")
+parser.add_argument("--channels", type=int, default=1, help="number of image channels")
+parser.add_argument("--sample_interval", type=int, default=400, help="interval betwen image samples")
+parser.add_argument("--save_dir", type=str, default="../../exp/bgan", help="interval between image sampling")
+parser.add_argument("--action", type=str, required=True)
+parser.add_argument("--data_path", type=str, default="", help="interval between image sampling")
+opt = parser.parse_args()
+print(opt)
+
+cuda = True if torch.cuda.is_available() else False
+
+
+def boundary_seeking_loss(y_pred, y_true):
+    """
+    Boundary seeking loss.
+    Reference: https://wiseodd.github.io/techblog/2017/03/07/boundary-seeking-gan/
+    """
+    return 0.5 * torch.mean((torch.log(y_pred) - torch.log(1 - y_pred)) ** 2)
+
+
+discriminator_loss = torch.nn.BCELoss()
+
+# Initialize generator and discriminator
+generator = Generator(opt.latent_dim)
+discriminator = Discriminator(opt.latent_dim)
+
+if cuda:
+    generator.cuda()
+    discriminator.cuda()
+    discriminator_loss.cuda()
+
+dataset = KPSDataset(opt.data_path, opt.action)
+dataloader = torch.utils.data.DataLoader(dataset, batch_size=opt.batch_size, shuffle=True)
+
+# Optimizers
+optimizer_G = torch.optim.Adam(generator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=opt.lr, betas=(opt.b1, opt.b2))
+
+Tensor = torch.cuda.FloatTensor if cuda else torch.FloatTensor
+
+
+def sample_kps(n_row, batches_done, save_dir):
+    os.makedirs(save_dir, exist_ok=True)
+
+    color_dict = [(0, 255, 0), (0, 0, 255), (255, 0, 0), (0, 255, 255), (255, 255, 0)]
+
+    connections = [(0, 1), (0, 2), (0, 3), (0, 4), (0, 5), (0, 6), (1, 3), (2, 4), (3, 4), (5, 6), (5, 7), (7, 9), (6, 8), (8, 10), (11, 12), (11, 13), (13, 15), (12, 14), (14, 16), (5, 11), (6, 12)]
+
+    def choose_color(coord_i):
+        if coord_i <= 4:
+            return color_dict[0]
+        elif coord_i in [5, 7, 9]:
+            return color_dict[1]
+        elif coord_i in [6, 8, 10]:
+            return color_dict[2]
+        elif coord_i in [11, 13, 15]:
+            return color_dict[3]
+        elif coord_i in [12, 14, 16]:
+            return color_dict[4]
+
+    z = Variable(Tensor(np.random.normal(0, 1, (n_row ** 2, opt.latent_dim))))
+    gen_kps = generator(z)
+    imgs = []
+    for gen_kp in gen_kps:
+        image = np.zeros((400, 400, 3), dtype=np.uint8)
+        float_single_coord = [x * 400 for x in gen_kp]
+        # print(label)
+        for i in range(17):
+            x = int(float_single_coord[i * 2])
+            y = int(float_single_coord[i * 2 + 1])
+            cv2.circle(image, (x, y), 5, choose_color(i), -1)
+
+        for i, j in connections:
+            x1, y1 = int(float_single_coord[i * 2]), int(float_single_coord[i * 2 + 1])
+            x2, y2 = int(float_single_coord[j * 2]), int(float_single_coord[j * 2 + 1])
+            cv2.line(image, (x1, y1), (x2, y2), choose_color(i), 2)
+        imgs.append(image)
+
+    concated_imgs = []
+    for i in range(n_row):
+        horizontal = np.concatenate(imgs[i*n_row:(i+1)*n_row], axis=1)
+        concated_imgs.append(horizontal)
+    concated_imgs = np.concatenate(concated_imgs, axis=0)
+    cv2.imwrite(os.path.join("{}/{}.png").format(save_dir, batches_done), concated_imgs)
+
+
+for epoch in range(opt.n_epochs):
+    for i, kps in enumerate(dataloader):
+
+        # Adversarial ground truths
+        valid = Variable(Tensor(kps.shape[0], 1).fill_(1.0), requires_grad=False)
+        fake = Variable(Tensor(kps.shape[0], 1).fill_(0.0), requires_grad=False)
+
+        # Configure input
+        real_imgs = Variable(kps.type(Tensor))
+
+        # -----------------
+        #  Train Generator
+        # -----------------
+
+        optimizer_G.zero_grad()
+
+        # Sample noise as generator input
+        z = Variable(Tensor(np.random.normal(0, 1, (kps.shape[0], opt.latent_dim))))
+
+        # Generate a batch of images
+        gen_imgs = generator(z)
+
+        # Loss measures generator's ability to fool the discriminator
+        g_loss = boundary_seeking_loss(discriminator(gen_imgs), valid)
+
+        g_loss.backward()
+        optimizer_G.step()
+
+        # ---------------------
+        #  Train Discriminator
+        # ---------------------
+
+        optimizer_D.zero_grad()
+
+        # Measure discriminator's ability to classify real from generated samples
+        real_loss = discriminator_loss(discriminator(real_imgs), valid)
+        fake_loss = discriminator_loss(discriminator(gen_imgs.detach()), fake)
+        d_loss = (real_loss + fake_loss) / 2
+
+        d_loss.backward()
+        optimizer_D.step()
+
+        print(
+            "[Epoch %d/%d] [Batch %d/%d] [D loss: %f] [G loss: %f]"
+            % (epoch, opt.n_epochs, i, len(dataloader), d_loss.item(), g_loss.item())
+        )
+
+        batches_done = epoch * len(dataloader) + i
+        if batches_done % opt.sample_interval == 0:
+            sample_kps(n_row=int(math.sqrt(kps.shape[0])), batches_done=batches_done,
+                       save_dir=os.path.join(opt.save_dir, "image"))
+
+    if epoch % 10 == 0:
+        torch.save(generator.state_dict(), os.path.join(opt.save_dir, "generator.pth"))
+        torch.save(discriminator.state_dict(), os.path.join(opt.save_dir, "discriminator.pth"))
---
+++ pose/bgan/models.py
@@ -0,0 +1,51 @@
+from torch import nn
+import torch
+import numpy as np
+from torch.autograd import Variable
+
+Tensor = torch.cuda.FloatTensor if torch.cuda.is_available() else torch.FloatTensor
+
+
+class Generator(nn.Module):
+    def __init__(self, latent_dim):
+        super(Generator, self).__init__()
+
+        def block(in_feat, out_feat, normalize=True):
+            layers = [nn.Linear(in_feat, out_feat)]
+            if normalize:
+                layers.append(nn.BatchNorm1d(out_feat, 0.8))
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            return layers
+
+        self.model = nn.Sequential(
+            *block(latent_dim, 128, normalize=False),
+            *block(128, 256),
+            *block(256, 512),
+            *block(512, 1024),
+            nn.Linear(1024, 34),
+            nn.Tanh()
+        )
+
+    def forward(self, z):
+        kps_gen = self.model(z)
+        return kps_gen
+
+
+class Discriminator(nn.Module):
+    def __init__(self, latent_dim):
+        super(Discriminator, self).__init__()
+
+        self.model = nn.Sequential(
+            nn.Linear(34, 512),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(512, 256),
+            nn.LeakyReLU(0.2, inplace=True),
+            nn.Linear(256, 1),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, z):
+        # img_flat = img.view(img.shape[0], -1)
+        # validity = self.model(img_flat)
+        validity = self.model(z)
+        return validity
